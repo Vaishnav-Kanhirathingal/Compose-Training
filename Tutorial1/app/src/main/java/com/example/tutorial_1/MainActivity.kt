9009f416e674127e5717926155aac29620c0cecf
Vaishnav Kanhirathingal <vaishnav.kanhira@gmail.com>
2022-08-12 18:49:59 +0530

    small changes

--- Tutorial1/app/src/main/java/com/example/tutorial_1/MainActivity.kt
+++ Tutorial1/app/src/main/java/com/example/tutorial_1/MainActivity.kt
@@ -3,15 +3,12 @@ package com.example.tutorial_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -50,29 +47,31 @@ fun myApp() {
 
 @Composable
 fun Greeting(name: String) {
-    var expanded = false
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 64.dp else 32.dp
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 4.dp)
     ) {
         Row(
             modifier = Modifier
-                .padding(24.dp)
                 .fillMaxWidth()
+                .padding(24.dp)
         ) {
             Column(
-                modifier = Modifier.weight(1f)
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
             ) {
                 Text(text = "Hello, ")
                 Text(text = name)
             }
             OutlinedButton(
                 onClick = {
-                    expanded = !expanded
-                    /*TODO*/
+                    expanded.value = !expanded.value
                 }
             ) {
-                Text(text = if (expanded) "show less" else "show more")
+                Text(text = if (expanded.value) "show less" else "show more")
             }
         }
     }
@@ -83,5 +82,37 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     Tutorial1Theme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun OnboardingScreen() {
+    // TODO: This state should be hoisted
+    val shouldShowOnBoarding = remember {
+        mutableStateOf(true)
+    }
+
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = { shouldShowOnBoarding.value = false }
+            ) {
+                Text("Continue")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    Tutorial1Theme {
+        OnboardingScreen()
     }
 }
