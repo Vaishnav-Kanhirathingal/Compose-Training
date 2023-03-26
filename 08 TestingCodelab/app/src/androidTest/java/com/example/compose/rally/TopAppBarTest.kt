1d5eb7555182484eb3a72557bae0c42685c28453
Vaishnav Kanhirathingal <vaishnav.kanhira@gmail.com>
2023-03-26 17:14:35 +0530

    tests with infinite animation written

--- 08 TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ 08 TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -1,8 +1,9 @@
 package com.example.compose.rally
 
-import androidx.compose.ui.test.assertIsSelected
+import androidx.compose.ui.test.hasParent
+import androidx.compose.ui.test.hasStateDescription
+import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithContentDescription
 import com.example.compose.rally.ui.components.RallyTopAppBar
 import org.junit.Rule
 import org.junit.Test
@@ -21,9 +22,10 @@ class TopAppBarTest {
                 currentScreen = RallyScreen.Accounts
             )
         }
-        Thread.sleep(5000)
-        composeTestRule
-            .onNodeWithContentDescription(RallyScreen.Accounts.name)
-            .assertIsSelected()
+        composeTestRule.onNode(
+            hasText(RallyScreen.Accounts.name.uppercase()) and
+                    hasParent(hasStateDescription(RallyScreen.Accounts.name)),
+            useUnmergedTree = true
+        ).assertExists()
     }
 }
